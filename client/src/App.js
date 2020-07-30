1518708f48c217f93baa89498dfffb8fe6897f24
johnnytortorici <johnny.tortorici@gmail.com>
2020-07-30 13:44:31 -0400

    Top-level components & routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import HomeFeed from "./components/HomeFeed";
+import Notifications from "./components/Notifications";
+import Bookmarks from "./components/Bookmarks";
+import TweetDetails from "./components/TweetDetails";
+import Profile from "./components/Profile";
 
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <HomeFeed />
+        </Route>
+        <Route path="/notifications">
+          <Notifications />
+        </Route>
+        <Route path="/bookmarks">
+          <Bookmarks />
+        </Route>
+        <Route path="/tweet/:tweetId">
+          <TweetDetails />
+        </Route>
+        <Route path="/:profileId">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
